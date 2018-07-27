549d785e3cdec5e8b8cd20f28f67791647550656
kiddspazz <travishorton.pianist@gmail.com>
2018-07-26 22:23:50 -0700

    added dots and movement

--- gifs.js
+++ gifs.js
@@ -5,13 +5,10 @@ const colors = [
 	'rgb(255, 0, 0)',
 	'rgb(255, 127, 0)',
 	'rgb(255, 255, 0)',
-	'rgb(127, 255, 0)',
-	'rgb(0, 255, 127)',
+	'rgb(0, 255, 0)',
 	'rgb(0, 255, 255)',
-	'rgb(0, 127, 255)',
 	'rgb(0, 0, 255)',
-	'rgb(127, 0, 255)',
-	'rgb(255, 0, 127)'
+	'rgb(127, 0, 255)'
 ];
 let canvas = document.getElementById('canvas');
 canvas.width = W;
@@ -23,44 +20,56 @@ ctx.translate(0, H);
 ctx.scale(1, -1);
 
 let u = 80;
+let circum = 2 * Math.PI
 let cycle = 0;
 ctx.lineWidth = 3;
 
 function draw() {
-	for (let sides = 0; sides < 12; sides ++) {
+	ctx.clearRect(0, 0, W, H);
+	for (let sides = 1; sides < 12; sides ++) {
 		ctx.beginPath();
 		for (let i = 0; i <= sides; i ++) {
-			ctx.lineTo(c.x + findX(i, sides) * sides/3, c.y + findY(i, sides) * sides/3);
+			let x = c.x + findX(i, sides) * sides/3;
+			let y = c.y + findY(i, sides) * sides/3;
+			ctx.lineTo(x, y);
 		};
-		ctx.strokeStyle = colors[(sides+cycle)%colors.length]
+		ctx.strokeStyle = colors[(sides)%colors.length] //(sides+cycle)%colors.length
 		ctx.stroke();
 
+		let dotX = c.x + findX(cycle%sides,sides) * sides/3;
+		let dotY = c.y + findY(cycle%sides,sides) * sides/3;
 		ctx.beginPath();
-		ctx.arc(
-			c.x + findX(0,sides) * sides/3,
-			c.y + findY(0,sides) * sides/3,
-			5,
-			0,
-			2 * Math.PI
-		);
+		ctx.arc(dotX, dotY, 7, 0, circum);
 		ctx.fillStyle = 'rgb(255,255,255)';
 		ctx.fill();
-	}
 
+	};
 	cycle ++;
+
 };
 
 function findX(i, sides) {
 	let mult = (2 * i/sides)
-	let answer = Math.round(10000 * Math.sin(mult * Math.PI))/10000;
+	let answer = Math.round(100000 * Math.sin(mult * Math.PI))/100000;
 	return answer * u;
-}
+};
 
 function findY(i, sides) {
 	let mult = (2 * i/sides);
-	let answer = Math.round(10000 * Math.cos(mult * Math.PI))/10000;
+	let answer = Math.round(100000 * Math.cos(mult * Math.PI))/100000;
 	return answer * u;
+};
+
+function distance(a, b) {
+	return (Math.sqrt(Math.pow((b[0] - a[0]), 2) + Math.pow((b[1] + a[1]), 2)));
+}
+
+function halfWayPoint(a, b) {
+	let c = [];
+	c[0] = a[0] + .5 * (b[0] - a[0]);
+	c[1] = a[1] + .5 * (b[1] - a[1]);
+	return c;
 }
 
 draw();
-window.setInterval(draw, 1000);
+window.setInterval(draw,1200);
